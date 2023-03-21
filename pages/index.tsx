18a02baeb6d717f7debe006fa696a430ac9ed9f4
nabil354 <nabil.voel354@gmail.com>
2023-03-21 22:12:22 +0700

    Fixed Typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,14 +46,14 @@ export default function Home() {
       <motion.section {...contentAnimation} className="relative order-1 w-1/3 md:order-2">
         <h3 className="-mb-5 text-base font-normal mb-0">Hi there! i&apos;m...</h3>
         <h1 className="-ml-1 text-7xl font-bold text-primary-light">xzhndvs</h1>
-        <h4 className="text-base font-light transition-all duration-500 hover:text-primary-light">
+        <h4 className="text-base font-light transition-all duration-500 mb-4 hover:text-primary-light">
         Technology Enthusiasts
         </h4>
-        <button
-              type="submit"
-              className="cursor-none mt-4 rounded-md border border-secondary-light bg-primary-dark px-4 py-1 font-semibold transition duration-500 ease-in-out placeholder:text-secondary-light hover:border-primary-light hover:bg-secondary-dark hover:text-primary-light hover:outline-none focus:border-primary-light focus:bg-secondary-dark focus:text-primary-light focus:outline-none">
+        <a
+              href="https://google.com"
+              className="cursor-none rounded-md border border-secondary-light bg-primary-dark px-4 py-1 font-semibold transition duration-500 ease-in-out placeholder:text-secondary-light hover:border-primary-light hover:bg-secondary-dark hover:text-primary-light hover:outline-none focus:border-primary-light focus:bg-secondary-dark focus:text-primary-light focus:outline-none">
               Download CV
-            </button>
+            </a>
       </motion.section>
     </main>
   );

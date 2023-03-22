43ca9dbdfaf65d54eef4c5d6fa2b80db393b2142
nabil354 <nabil.voel354@gmail.com>
2023-03-22 12:00:20 +0700

    Add API TrueCaller

--- pages/api/truecaller.ts
+++ pages/api/truecaller.ts
@@ -2,27 +2,38 @@
 import * as truecallerjs from 'truecallerjs';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse): void {
-  const nomorCode = req.query.nomorCode as string | undefined;
-  const countryCode = req.query.countryCode as string | undefined;
-  const creator = "xzhndvs";
+interface MyResponse {
+  status: string;
+  message: string;
+}
 
-  if (!nomorCode) return res.status(400).json({ error: `Input Parameter Nomor` });
-  if (!countryCode) return res.status(400).json({ error: `Input Parameter Country Code Example ID / MY` });
+export default async function handler(req: NextApiRequest, res: NextApiResponse<MyResponse>): Promise<void> {
+  if (req.method !== 'GET') {
+    return res.status(405).json({
+      status: 'error',
+      message: 'Method is not allowed!',
+    });
+  }
+
+  const nomorCode = req.query.nomorCode as string;
+  const countryCode = req.query.countryCode as string;
+  const creator = "xzhndvs";
 
   const searchData = {
     number: nomorCode,
     countryCode: countryCode,
     installationId: "a1i0N--ZtzzhIk_k_gN90VfTZjxmsTzt_5qiBX6pCXTp4EFfZUkZE6mDb6OlXQXR",
-  };
+  } as const;
 
-  truecallerjs.searchNumber(searchData).then(data => {
-      res.status(200).json({
-        creator: `${creator}`,
-        status: true,
-        result: data
-      });
-    }).catch(() => {
-      res.status(500).json({ error: 'failed to load data' });
+  try {
+    const data = await truecallerjs.searchNumber(searchData);
+    // @ts-ignore
+    res.status(200).json({ status: 'ok', message: 'Success!', data: data });
+  } catch (err: unknown) {
+    res.status(500).json({
+      status: 'error',
+      // @ts-ignore
+      message: typeof err === 'string' ? err : err?.message || 'Internal Server Error',
     });
-};
+  }
+}

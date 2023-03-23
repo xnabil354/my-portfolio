84a83a1feee9c35ac8e38207a882dcfaa3cd8bb7
nabil354 <nabil.voel354@gmail.com>
2023-03-23 15:22:38 +0700

    Fixed Bugs

--- pages/api/truecaller.ts
+++ pages/api/truecaller.ts
@@ -3,34 +3,22 @@ import * as truecallerjs from 'truecallerjs';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 interface MyResponse {
-  creator: string;
   status: string;
   message: string;
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<MyResponse>): Promise<void> {
-  const response: MyResponse = {
-    creator: 'xzhndvs',
-    status: 'success',
-    message: 'Your request was successful!'
-  };
-  // @ts-ignore
-  if (req.method !== 'GET') {
-    return res.status(405).json({
-      status: 'error',
-      message: 'Method is not allowed!',
-    });
-  }
-
+  // Check if nomorCode and countryCode are provided
   const nomorCode = req.query.nomorCode as string;
   const countryCode = req.query.countryCode as string;
-
   if (!nomorCode || !countryCode) {
     return res.status(400).json({
       status: 'error',
-      message: 'Invalid request. Please provide both nomorCode and countryCode.',
+      message: 'Both nomorCode and countryCode are required!',
     });
   }
+
+  const creator = "xzhndvs";
 
   const searchData = {
     number: nomorCode,
@@ -41,8 +29,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   try {
     const data = await truecallerjs.searchNumber(searchData);
     
+    // Return success response with the response and data
+    const response: MyResponse = {
+      status: 'success',
+      message: 'Your request was successful!'
+    };
     // @ts-ignore
-    res.status(200).json({ response, data: data });
+    res.status(200).json({ creator, response, data: data });
   } catch (err: unknown) {
     res.status(500).json({
       status: 'error',
